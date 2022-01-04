0204f29cfc198265cfb89fc0b84d24826b1a3726
chanyoung <cyl0504@gmail.com>
2022-01-05 01:52:58 +0900

    ADD : annotate, reverse relation, error message

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views      import CategoryMainView
+from .views      import CategoryMainView, ListPageView
 
 urlpatterns = [
     path('/categories',CategoryMainView.as_view()),
+    path('/list',ListPageView.as_view()),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,11 +1,12 @@
 import json
 
-from django.http        import JsonResponse
-from django.views       import View
-from json.decoder       import JSONDecodeError
+from django.db.models.aggregates import Avg, Count
 
-from restaurants.models import Category, ImageCategory, Restaurant, ImageRestaurant
-from reviews.models     import Review
+from django.http                 import JsonResponse
+from django.views                import View
+from json.decoder                import JSONDecodeError
+
+from restaurants.models          import Category, ImageCategory, Restaurant, ImageRestaurant
 
 class CategoryMainView(View):
     def get(self, request):
@@ -28,7 +29,31 @@ class ListPageView(View):
     def get(self, request):
         try:
             category_name = request.GET['category']
-            sorts = request.GET.get('sort','character')
-            category = Category.objects.get(name=category_name)
-            restaurants = category.restaurant_set.all()
-            ratings
+            sorts         = request.GET.get('sort','name')
+            category      = Category.objects.get(name=category_name)
+            restaurants   = Restaurant\
+                          .objects\
+                          .annotate(avg_rating=Avg('review__rating'), review_count=Count('review'))\
+                          .filter(category=category)\
+                          .order_by(sorts)
+            result = [{
+                'id'      : restaurant.id,
+                'name'    : restaurant.name,
+                'rating'  : restaurant.avg_rating,
+                'reviews' : restaurant.review_count,
+                'image'   : restaurant.imagerestaurant_set.first().url,
+            }for restaurant in restaurants]
+
+            return JsonResponse({'result' : result}, status=200)
+        
+        except JSONDecodeError:
+            return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status=400)
+
+        except Category.DoesNotExist:
+            return JsonResponse({'message' : 'Category_DoesNotExist'}, status=404)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEYERROR'}, status=401)
+
+        except:
+            return JsonResponse({'message' : 'FAILED'}, status=400)

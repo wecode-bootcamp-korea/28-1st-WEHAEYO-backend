7df31b8b91a0c375ee38a276923001a9b186336a
chanyoung <cyl0504@gmail.com>
2022-01-04 14:42:46 +0900

    Modify : CategoryMainView, urls.py

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,15 +6,15 @@ from restaurants.models import Category, ImageCategory
 class CategoryMainView(View):
     def get(self, request):
         try:
-            categories=Category.objects.all()
-            food_category = []
-            for category in categories:
-                images=ImageCategory.objects.get(category=category)
-                food_category.append({
-                    'category_name':category.name,
-                    'image_url':images.url,
-                })
-            return JsonResponse({'RESULT' : food_category}, status=200) 
+            categories    = Category.objects.all()
+            food_category = [
+                {
+                    'category_name': category.name,
+                    'image_url'    : ImageCategory.objects.get(category=category).url,
+                }for category in categories
+            ]
+                
+            return JsonResponse({'result' : food_category}, status=200) 
             
         except:
             return JsonResponse({'Message' : 'FAILED'}, status=400)
